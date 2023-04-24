39003262c43036a2a61bab1bfdad83845d77d6bb
codms0201 <kellykwon58@handong.ac.kr>
2023-04-24 17:22:08 +0900

    modify loadData() in manager.c

--- manager.c
+++ manager.c
@@ -59,10 +59,16 @@ int loadData(Product *p){
 	FILE*fp;
 
 	//파일 내용을 읽어와서 배열에 값 추가하기
-
-
-
-
+	fp = fopen("product.txt", "rt");
+	if(fp == NULL){
+		printf("파일을 여는데 실패했습니다!\n");
+		return 0;
+	}
+	while(!feof(fp)){
+		fscanf(fp,"%s %d %d\n", p[count].name, &p[count].weight, &p[count].price);
+		count++;
+	}
+	fclose(fp);
 	printf("=> 로딩 성공!\n");
 	return count;
 }
